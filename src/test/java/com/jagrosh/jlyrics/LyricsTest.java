623940ea99174fa36f72dc8d0c42b349060236b3
Presti <dxssucuk@hotmail.com>
2024-10-31 13:38:31 +0100

    Remove MusixMatch support.

--- src/test/java/com/jagrosh/jlyrics/LyricsTest.java
+++ src/test/java/com/jagrosh/jlyrics/LyricsTest.java
@@ -89,39 +89,6 @@ public class LyricsTest
     }
     
     @Test
-    public void musixmatchTest() throws InterruptedException, ExecutionException
-    {
-        LyricsClient client = new LyricsClient("MusixMatch");
-        
-        Lyrics lyrics = client.getLyrics("ellie goulding lights").get();
-        assertNotNull(lyrics);
-        assertNotNull(lyrics.getTitle());
-        assertNotNull(lyrics.getAuthor());
-        assertNotNull(lyrics.getContent());
-        assertNotNull(lyrics.getSource());
-        
-        lyrics = client.getLyrics("jklsjdgv89y32hr9").get();
-        assertNull(lyrics);
-        
-        lyrics = client.getLyrics("smooth criminal").get();
-        assertNotNull(lyrics);
-        assertNotNull(lyrics.getTitle());
-        assertNotNull(lyrics.getAuthor());
-        assertNotNull(lyrics.getContent());
-        assertNotNull(lyrics.getSource());
-    }
-    
-    @Test
-    public void musicmatchTest() throws InterruptedException, ExecutionException
-    {
-        LyricsClient client = new LyricsClient("MusicMatch");
-        
-        Lyrics lyrics = client.getLyrics("smooth criminal").get();
-        assertNotNull(lyrics);
-        assertNotNull(lyrics.getTitle());
-    }
-    
-    @Test
     public void lyricsfreakTest() throws InterruptedException, ExecutionException
     {
         LyricsClient client = new LyricsClient("LyricsFreak");
